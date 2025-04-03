aaf8528fcca74f2086d4f33451b9bd815285da6b
niklasovaska <niklas.ovaska@gmail.com>
2025-04-03 19:24:12 +0300

    card favorite icon extracted to own component

---
+++ src/components/CardFavoriteIcon.jsx
@@ -0,0 +1,19 @@
+import handleFavoriteUpdate from '../hooks/handleFavoriteUpdate'
+import IconButton from '@mui/material/IconButton'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import Tooltip from '@mui/material/Tooltip'
+
+
+const CardFavoriteIcon = ({ book, books, setBooks }) => {
+
+    return(
+        <Tooltip title={book.favorite ? `` : `Add to favorites`}>
+            <IconButton onClick={() => handleFavoriteUpdate(book.id, books, setBooks)}>
+                {book.favorite ? <FavoriteIcon fontSize='small'/> : <FavoriteBorderIcon fontSize='small'/>}
+            </IconButton>
+        </Tooltip>
+    )
+}
+
+export default CardFavoriteIcon
