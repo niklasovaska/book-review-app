2dba6bc1157c1312ee5e9674bc10f2da2269af2f
niklasovaska <niklas.ovaska@gmail.com>
2025-04-04 15:17:55 +0300

    add book review functionality

---
+++ src/components/AddBookModal.jsx
@@ -0,0 +1,24 @@
+import Form from './Form'
+import modalStyle from '../utils/modalStyles'
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
+
+const AddBookModal = ({ openModal, handleClose, books, setBooks, setAlert }) => {
+
+    return(
+        <Modal
+            open={openModal}
+            onClose={handleClose}>
+            <Box sx={modalStyle}>
+                <Form
+                    books={books} 
+                    setBooks={setBooks} 
+                    handleClose={handleClose} 
+                    setAlert={setAlert}
+                />
+            </Box>
+        </Modal>
+    )
+}
+
+export default AddBookModal
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,10 @@
-import {Box, Button, Container,FormControl, InputLabel, MenuItem, OutlinedInput, Paper, Rating, Select, TextField, Typography} from '@mui/material'
+import {Box, Button, Container,FormControl, InputLabel, MenuItem, OutlinedInput, Rating, Select, TextField, Typography} from '@mui/material'
+import bookService from '../services/books'
 import { useState } from 'react'
 import genreList from '../data/listOfGenres'
 
-const Form = ({ books, setBooks }) => {
+
+const Form = ({ books, setBooks, handleClose, setAlert }) => {
 
     const [form, setForm] = useState({
         name: '',
@@ -28,87 +30,98 @@ const Form = ({ books, setBooks }) => {
             typeof value === 'string' ? value.split(',') : value,
           )
     }
-
-    const handleAdd = (e) => {
+    
+    const handleAddBook = (e) => {
         e.preventDefault()
+        
         const newBook = {
-            ...form,
-            publicationYear: parseInt(form.publicationYear),
-            genres: selectedGenres,
-            ratings: [ratingValue],
-            id: books.length + 1}
-
-        setBooks(books.concat(newBook))
-        setForm({
-            name: '',
-            author: '',
-            publicationYear: 0
-        })
-    }
-
-    const styles = {
-
+                ...form,
+                publicationYear: parseInt(form.publicationYear),
+                genres: selectedGenres,
+                ratings: [ratingValue],
+                favorite: false,
+                challengeNumber: null
+            }
+        
+        bookService
+            .create(newBook)
+            .then(res => {
+                setBooks(books.concat(res.data))
+                setAlert({...alert, 
+                    visibility: true, 
+                    severity: 'success', 
+                    message: 'New book review added successfully!'
+                })
+            })
+            .catch(error => {
+                console.log(error)
+                setAlert({...alert, 
+                    visibility: true, 
+                    severity: 'error', 
+                    message: 'There was an error adding a book review'
+                })
+            })
+    
+        handleClose()
+    
+        setForm({name: '', author: '', publicationYear: 0})
     }
     
     return(
-        <Container sx={{mb: 10, width: 500}}>
-            <Paper>
-                <Typography sx={{padding: '2rem'}}variant='h5'>Review a new book</Typography>
-                <form onSubmit={handleAdd} 
-                style={{display: 'flex', flexDirection: 'column', gap: 15, padding: '2rem'}}>
-                    <TextField 
-                        label="Name"
-                        name="name"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        label="Author"
-                        name="author"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        label="Publication year"
-                        name="publicationYear"
-                        onChange={handleChange}
-                    />
-                    <Box>
-                        <FormControl sx={{width: '100%' }}>
-                            <InputLabel id="genres">Select genres</InputLabel>
-                            <Select
-                                labelId="genre-label"
-                                id="genres"
-                                multiple
-                                value={selectedGenres}
-                                onChange={handleGenreChange}
-                                input={<OutlinedInput label="Select genres" />}
+        <Container> 
+            <Typography sx={{padding: '2rem'}}variant='h5'>Review a new book</Typography>
+            <form onSubmit={handleAddBook} 
+            style={{display: 'flex', flexDirection: 'column', gap: 15, padding: '2rem'}}>
+                <TextField 
+                    label="Name"
+                    name="name"
+                    onChange={handleChange}
+                />
+                <TextField
+                    label="Author"
+                    name="author"
+                    onChange={handleChange}
+                />
+                <TextField
+                    label="Publication year"
+                    name="publicationYear"
+                    onChange={handleChange}
+                />
+                <Box>
+                    <FormControl sx={{width: '100%' }}>
+                        <InputLabel id="genres">Select genres</InputLabel>
+                        <Select
+                            labelId="genre-label"
+                            id="genres"
+                            multiple
+                            value={selectedGenres}
+                            onChange={handleGenreChange}
+                            input={<OutlinedInput label="Select genres" />}
+                        >
+                            {genreList.map((name) => (
+                            <MenuItem
+                                key={name}
+                                value={name}
                             >
-                                {genreList.map((name) => (
-                                <MenuItem
-                                    key={name}
-                                    value={name}
-                                >
-                                    {name}
-                                </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                    </Box>
-                    <Box sx={{display: 'flex', gap: 5}}>
-                        <InputLabel sx={{marginLeft: 2}}>Rating</InputLabel>
-                        <Rating name="rating" 
-                            precision={0.5}
-                            value={ratingValue}  
-                            onChange={(e, newValue) => {
-                                setRatingValue(newValue)
-                            }}
-                        />
-                    </Box>
-                    <Button type='submit' variant='outlined'>Add</Button>
-                </form>
-            </Paper>
+                                {name}
+                            </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Box sx={{display: 'flex', gap: 5}}>
+                    <InputLabel sx={{marginLeft: 2}}>Rating</InputLabel>
+                    <Rating name="rating" 
+                        precision={0.5}
+                        value={ratingValue}  
+                        onChange={(e, newValue) => {
+                            setRatingValue(newValue)
+                        }}
+                    />
+                </Box>
+                <Button type='submit' variant='outlined' color='secondary'>Add</Button>
+            </form>
         </Container>
-        
-        // <Box component="section" sx={{p: 5, margin: 10, border: '1px solid black' }}></Box>
     )
 }
 
