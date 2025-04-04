9c7690e31e4f23294bc4a299d35beb43a8df1f45
niklasovaska <niklas.ovaska@gmail.com>
2025-04-04 15:18:22 +0300

    navbar structure updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { AppBar, Toolbar, Typography } from '@mui/material/'
 import Box from '@mui/material/Box'
 import Drawer from '@mui/material/Drawer'
 import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem';
+import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
@@ -13,12 +13,23 @@ import FavoriteIcon from '@mui/icons-material/Favorite'
 import ElectricBoltIcon from '@mui/icons-material/ElectricBolt'
 import AddCircleIcon from '@mui/icons-material/AddCircle'
 import Tooltip from '@mui/material/Tooltip'
+import AddBookModal from './AddBookModal'
+import NotificationAlert from './NotificationAlert'
 import { Link, Outlet } from 'react-router'
 
 import { useState } from 'react'
 
 const Navbar = ({ books, setBooks }) => {
     const [DrawerOpen, setDrawerOpen] = useState(false)
+    const [openModal, setOpenModal] = useState(false)
+    const [alert, setAlert] = useState({
+        visibility: false,
+        severity: '',
+        message: ''
+    })
+
+    const handleOpen = () => setOpenModal(true);
+    const handleClose = () => setOpenModal(false);
 
     const toggleDrawer = (newOpen) => () => {
         setDrawerOpen(newOpen)
@@ -28,20 +39,20 @@ const Navbar = ({ books, setBooks }) => {
         return(
             <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
                 <List>
-                    <ListItem component={ Link } to='/'>
-                        <ListItemButton>
+                    <ListItem>
+                        <ListItemButton component={Link} to='/'>
                             <ListItemIcon><HomeIcon /></ListItemIcon>
                             <ListItemText primary='Home'/>
                         </ListItemButton>
                     </ListItem>
-                    <ListItem component={ Link } to='/favorites'>
-                        <ListItemButton>
+                    <ListItem>
+                        <ListItemButton component={Link} to='/favorites'>
                             <ListItemIcon><FavoriteIcon /></ListItemIcon>
                             <ListItemText primary='Favorites'/>
                         </ListItemButton>
                     </ListItem>
-                    <ListItem component={ Link } to='/challenge'>
-                        <ListItemButton>
+                    <ListItem>
+                        <ListItemButton component={Link} to='/challenge'>
                             <ListItemIcon><ElectricBoltIcon /></ListItemIcon>
                             <ListItemText primary='Reading challenge'/>
                         </ListItemButton>
@@ -51,7 +62,6 @@ const Navbar = ({ books, setBooks }) => {
         )
     }
 
-
     return(
         <>
             <Box sx={{flexGrow: 1, marginBottom: 10}}>
@@ -60,8 +70,17 @@ const Navbar = ({ books, setBooks }) => {
                         <IconButton onClick={toggleDrawer(true)}><MenuIcon /></IconButton>
                         <Typography variant='h5' sx={{marginLeft: 6, fontWeight: 'bold', flexGrow: 1}}>the book review club</Typography>
                         <Tooltip title='Add new course'>
-                            <IconButton><AddCircleIcon fontSize='large'/></IconButton>
+                            <IconButton onClick={handleOpen}>
+                                <AddCircleIcon fontSize='large'/>
+                            </IconButton>
                         </Tooltip>
+                        <AddBookModal 
+                            openModal={openModal} 
+                            handleClose={handleClose} 
+                            books={books}
+                            setBooks={setBooks}
+                            setAlert={setAlert}
+                        />
                     </Toolbar>
                 </AppBar>
                 <Drawer
@@ -70,6 +89,7 @@ const Navbar = ({ books, setBooks }) => {
                     <DrawerItems />
                 </Drawer>
             </Box>
+            <NotificationAlert alert={alert} setAlert={setAlert}/>
             <Outlet />
         </>
     )
