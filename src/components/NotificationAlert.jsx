07a205f25dbe4f052bf9c3bf7ccade9a22f2e043
niklasovaska <niklas.ovaska@gmail.com>
2025-04-07 16:06:03 +0300

    mui snackbar notification alert

--- src/components/NotificationAlert.jsx
+++ src/components/NotificationAlert.jsx
@@ -1,27 +1,23 @@
 import Alert from '@mui/material/Alert'
-import AlertTitle from '@mui/material/AlertTitle'
-import Fade from '@mui/material/Fade'
-import Box from '@mui/material/Box'
-import modalStyle from '../utils/modalStyles'
+import Snackbar from '@mui/material/Snackbar'
+
 
 const NotificationAlert = ({ alert, setAlert }) => {
 
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+        setAlert({...alert, visibility: false, severity: '', message: ''});
+      };
+
     return(
-        <Fade
-            in={alert.visibility}
-            addEndListener={() => {
-                setTimeout(() => {
-                    setAlert({...alert, visibility: false, severity: '', message: ''})
-                }, 3000)
-            }}
-            >
-            <Box sx={modalStyle}>
-                <Alert severity={alert.severity}>
-                    <AlertTitle>{alert.severity.charAt(0).toUpperCase() + alert.severity.slice(1)}</AlertTitle>
-                    {alert.message}
-                </Alert>
-            </Box>
-        </Fade>
+        <Snackbar open={alert.visibility} autoHideDuration={5000} onClose={handleClose}>
+            <Alert severity={alert.severity}>
+                {alert.message}
+            </Alert>
+        </Snackbar>
+    
     )
 }
 
