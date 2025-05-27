0e3bfaabd2837babb8d205e9a18948c0e9e4e5d7
niklasovaska <niklas.ovaska@gmail.com>
2025-05-27 10:08:55 +0300

    usequery and update props

--- src/pages/ReadingChallenge.jsx
+++ src/pages/ReadingChallenge.jsx
@@ -1,19 +1,31 @@
-import challengeList from '../data/listOfChallenges'
 import ChallengeGridList from '../components/ChallengeGridList'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import { useQuery } from '@tanstack/react-query'
+import { getAll } from '../services/books'
 
-const ReadingChallenge = ({ books, setBooks }) => {
+const ReadingChallenge = ({ setAlert }) => {
+
+    const { data: challenges, error, isLoading } = useQuery({
+        queryKey: ['challenges'],
+        queryFn: () => getAll('/challenges')
+    })
+
+    if(isLoading) {
+    return <span>Loading...</span>
+    }
+
+    if(error) {
+    return(<span>Error when fetching challenges...</span>)
+    }
+
     return(
         <>
             <Box component="section" sx={{p: 6, mx: 10}}>
                 <Typography variant='h3'>Reading Challenge 2025</Typography>
+                <Typography variant='h6'>Complete all 50 reading challenges!</Typography>
             </Box>
-            <ChallengeGridList 
-                challenges={challengeList} 
-                books={books} 
-                setBooks={setBooks}
-            />
+            <ChallengeGridList challenges={challenges} setAlert={setAlert} />
         </>
     )
 }
