818e178ed26c898c8956cbce98a69df5a6b1c356
niklasovaska <niklas.ovaska@gmail.com>
2025-05-27 10:01:04 +0300

    usequery

--- src/pages/AllBooks.jsx
+++ src/pages/AllBooks.jsx
@@ -2,25 +2,41 @@ import BookGridList from '../components/BookGridList'
 import Statistics from '../components/Statistics'
 import Searchbox from '../components/Searchbox'
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 import { useState } from 'react'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAll } from '../services/books'
 
-const AllBooks = ({ books, setBooks }) => {
+const AllBooks = ({ setAlert }) => {
 
     const [searchValue, setSearchValue] = useState('')
 
     const handleSearchValueChange = (e) => {
         setSearchValue(e.target.value)
-      }
+    }
+
+    const { data: books, error, isLoading } = useQuery({
+          queryKey: ['books'],
+          queryFn: () => getAll('/books')
+    })
+
+    if(isLoading) {
+        return <span>Loading...</span>
+    }
+    if(error) {
+        return(<span>Error when fetching books...</span>)
+    }
 
     const booksToMap = books.filter((book) => book.name.toLowerCase().includes(searchValue.toLowerCase()) || book.author.toLowerCase().includes(searchValue.toLowerCase()))
 
     return(
         <>
             <Statistics books={books} />
-            <Box component='section' sx={{p: 1, m: 10}}>
+            <Box component='section' sx={{p: 4, mx: 10}}>
+                <Typography variant='h4'>Reviews</Typography>
                 <Searchbox handleSearchValueChange={handleSearchValueChange} />
             </Box>
-            <BookGridList booksToMap={booksToMap} books={books} setBooks={setBooks} />
+            <BookGridList booksToMap={booksToMap} books={books} setAlert={setAlert} />
         </>
     )
 }
