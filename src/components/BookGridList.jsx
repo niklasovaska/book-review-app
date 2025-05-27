50365825472931247242f926d6ff741da67fd8c0
niklasovaska <niklas.ovaska@gmail.com>
2025-05-27 10:07:37 +0300

    update props

--- src/components/BookGridList.jsx
+++ src/components/BookGridList.jsx
@@ -2,13 +2,13 @@ import Grid from '@mui/material/Grid2'
 import Box from '@mui/material/Box'
 import BookCard from './BookCard'
 
-const BookGridList = ({ booksToMap, books, setBooks }) => {
+const BookGridList = ({ booksToMap, books, setAlert }) => {
     return(
-        <Box component="section" sx={{mx: 10}}>
+        <Box component="section" sx={{mx: 10, mb: 10}}>
             <Grid container spacing={{xs: 2, md: 3}} columns={{xs: 4, sm: 8, md: 12}}>
                 {booksToMap.map(book => 
                     <Grid key={book.id} size={{xs: 4}}>
-                        <BookCard book={book} books={books} setBooks={setBooks}/>
+                        <BookCard book={book} books={books} setAlert={setAlert} />
                     </Grid>)}
             </Grid>
         </Box>
