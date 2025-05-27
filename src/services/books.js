eefb74a398bc8f3c844e913dc6915a96fd21c6cf
niklasovaska <niklas.ovaska@gmail.com>
2025-05-27 09:49:45 +0300

    using deb back-end url

--- src/services/books.js
+++ src/services/books.js
@@ -1,20 +1,43 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3000/books'
+const baseUrl = 'http://localhost:3001/api'
+const bookUrl = 'http://localhost:3001/api/books'
+const challengeUrl = 'http://localhost:3001/api/challenges'
 
-const getAll = () => {
-    return axios.get(baseUrl)
+export const getAll = async (url) => {
+    const response = await axios.get(`${baseUrl}${url}`)
+    return response.data
 }
 
-const create = newBook => {
-    return axios.post(baseUrl, newBook)
+export const createBook =  async (newBook) => {
+    const response = await axios.post(bookUrl, newBook)
+    return response.data
 }
 
-const update = (id, newBook) => {
-    return axios.put(`${baseUrl}/${id}`, newBook)
+export const updateBook = async (updatedBook) => {
+    const response = await fetch(`${bookUrl}/${updatedBook.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedBook)
+    })
+    return response.json()
+
 }
 
-const deleteBook = id => {
-    return axios.delete(`${baseUrl}/${id}`)
+export const deleteBook = async (id) => {
+    const response = await axios.delete(`${bookUrl}/${id}`)
+    return response.data
 }
 
-export default {getAll, create, update, deleteBook}
+export const updateChallenge = async (updatedChallenge) => {
+    const response = await fetch(`${challengeUrl}/${updatedChallenge.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedChallenge)
+    })
+    return response.json()
+}
+
