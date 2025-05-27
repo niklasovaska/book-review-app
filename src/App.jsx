5ecccfa8ebd4ddccd2f1e6dc4f94e2db827cd535
niklasovaska <niklas.ovaska@gmail.com>
2025-05-27 10:02:41 +0300

    alert state for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import useFetch from './hooks/useFetch'
 import theme from './utils/theme'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
-import { Typography } from '@mui/material'
 import Navbar from './components/Navbar'
 import AllBooks from './pages/AllBooks'
 import Favorites from './pages/Favorites'
@@ -10,37 +8,34 @@ import ReadingChallenge from './pages/ReadingChallenge'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { useState } from 'react'
 
+
 function App() {
 
-  const [books, setBooks, error, loading] = useFetch()
   const [darkMode, setDarkMode] = useState(true) 
-
-  if(error) {
-    return <Typography>Something went wrong</Typography>
-  }
-
-  if(loading) {
-    return <Typography>Loading...</Typography>
-  }
+  const [alert, setAlert] = useState({
+    visibility: false,
+    severity: '',
+    message: ''
+})
 
   return (
     <ThemeProvider theme={darkMode ? theme.darkTheme : theme.lightTheme}>
       <CssBaseline />
       <BrowserRouter>
-        <Routes>
-          <Route 
-            path='/' 
-            element=
-              {<Navbar 
-                books={books} 
-                setBooks={setBooks} 
-                darkMode={darkMode} 
-                setDarkMode={setDarkMode} />}>
-            <Route index element={<AllBooks books={books} setBooks={setBooks} />} />
-            <Route path='/favorites' element={<Favorites books={books} setBooks={setBooks} />} />
-            <Route path='/challenge' element={<ReadingChallenge books={books} setBooks={setBooks}/>} />
-          </Route>
-        </Routes>
+          <Routes>
+            <Route 
+              path='/' 
+              element=
+                {<Navbar
+                  alert={alert} 
+                  setAlert={setAlert} 
+                  darkMode={darkMode} 
+                  setDarkMode={setDarkMode} />}>
+              <Route index element={<AllBooks setAlert={setAlert}/>} />
+              <Route path='/favorites' element={<Favorites />} />
+              <Route path='/challenge' element={<ReadingChallenge setAlert={setAlert}/>} />
+            </Route>
+          </Routes>
       </BrowserRouter>
     </ThemeProvider>
   )
