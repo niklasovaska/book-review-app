74477b5f09ee2bc5a79d9fbbf7339581603c6a5a
niklasovaska <niklas.ovaska@gmail.com>
2025-05-27 10:09:16 +0300

    usequery

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,19 +1,33 @@
 import BookGridList from '../components/BookGridList'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import { useQuery } from '@tanstack/react-query'
+import { getAll } from '../services/books'
 
+const Favorites = () => {
 
-const Favorites = ({ books, setBooks }) => {
-
-    const favorites = books.filter((book) => book.favorite === true)
     
+    
+    const { data: books, error, isLoading } = useQuery({
+              queryKey: ['books'],
+              queryFn: () => getAll('/books')
+        })
+    
+    if(isLoading) {
+        return <span>Loading...</span>
+    }
+    if(error) {
+        return(<span>Error when fetching books...</span>)
+    }
+
+    const favorites = books.filter((book) => book.favorite === 1)
 
     return(
         <>
             <Box component="section" sx={{p: 6, mx: 10}}>
                 <Typography variant='h3'>Favorites</Typography>
             </Box>
-            <BookGridList booksToMap={favorites} books={books} setBooks={setBooks} />
+            <BookGridList booksToMap={favorites} books={books} />
         </>
     )
 }
